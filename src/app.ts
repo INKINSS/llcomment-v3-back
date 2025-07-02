9f917bf5fa2bde6ac21cb7ae2322c0c9bcc19843
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-07-02 16:35:54 -0600

    feat: :rocket: añade rutas publicas a archivo principal

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import { publicRoutes } from "./routes";
 
 const app = express();
+
+// Public routes
+app.use("/", publicRoutes);
 
 app.get("/", (_req, res) => {
 	res.send("Hello World!");
