2daf2de158bf86730dec7a3f5c2de11486235c92
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-07-02 19:14:24 -0600

    feat: :rocket: ejecuta middleware de mongoDB
    añade configuración para usar variables de entorno de desarrollo y producción

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,20 @@
+import dotenv from "dotenv";
 import express from "express";
+import { connectDB } from "./db/config";
 import { publicRoutes } from "./routes";
 
+//middlewares
+dotenv.config({
+	path:
+		process.env.NODE_ENV === "production"
+			? ".env.production.local"
+			: ".env.development.local",
+});
+
 const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 // Public routes
 app.use("/", publicRoutes);
