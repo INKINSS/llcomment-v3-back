c99c1d18d5afb2aca15699282d607e2bfde18972
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-07-03 11:02:39 -0600

    añade modelo para apartado de comentarios

---
+++ src/models/comments.model.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+export const commentSchema = new Schema(
+	{
+		postId: { type: String, required: true, default: () => uuidv4() },
+		authorId: { type: String, required: true },
+		content: { type: String, required: true },
+		createdAt: { type: Date, default: Date.now },
+		updatedAt: { type: Date, default: Date.now },
+	},
+	{
+		versionKey: false,
+	},
+);
+
+const CommentModel = mongoose.model('Comment', commentSchema, 'comments');
+
+export default CommentModel;
