6adf23c74394dbb8ca151bf5423bfa7c7181b77a
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-07-03 11:04:07 -0600

    feat: añade modelo para apartado de post

---
+++ src/models/posts.model.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from 'mongoose';
+
+export const postSchema = new Schema(
+	{
+		authorId: { type: String, required: true },
+		title: { type: String, required: true },
+		content: { type: String, required: true },
+		tags: { type: [String], required: true },
+		createdAt: { type: Date, default: Date.now },
+		updatedAt: { type: Date, default: Date.now },
+	},
+	{
+		versionKey: false,
+	},
+);
+
+const PostModel = mongoose.model('Post', postSchema, 'posts');
+
+export default PostModel;
